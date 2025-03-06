c15ff9bc8206615d06b9358ea54bc20d105baa7b
jokerxin <jxpro@qq.com>
2025-03-06 20:08:56 +0800

    feat: :sparkles: Add upload state module and test

---
+++ state/state.go
@@ -0,0 +1,35 @@
+package state
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type State struct {
+	// FilePath 表示待上传文件的本地路径
+	FilePath string `json:"file_path"`
+	// FileSize 表示文件的总大小（单位：字节）
+	FileSize int64 `json:"file_size"`
+	// UploadID 用于标识当前的 multipart upload 任务
+	UploadID string `json:"upload_id"`
+	// TrunkSize 表示每个分块的大小（单位：字节）
+	TrunkSize int `json:"trunk_size"`
+	// TrunkLength 表示每个分块的大小（单位：字节）
+	TrunkLength int `json:"trunk_length"`
+}
+
+func (s *State) Save() error {
+	data, err := json.Marshal(s)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(s.FilePath+".upload.state", data, 0644)
+}
+
+func LoadState(stateFile []byte) (*State, error) {
+	var s State
+	if err := json.Unmarshal(stateFile, &s); err != nil {
+		return nil, err
+	}
+	return &s, nil
+}
---
+++ state/state_test.go
@@ -0,0 +1,41 @@
+package state
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSaveAndLoadState(t *testing.T) {
+	tmpFile, err := os.CreateTemp("", "temp1g_*")
+	require.NoError(t, err, "文件创建失败")
+	defer func() {
+		_ = os.Remove(tmpFile.Name())
+	}()
+
+	const oneGB = int64(1024 * 1024 * 1024)
+	err = tmpFile.Truncate(oneGB)
+	assert.NoError(t, err, "文件截断失败")
+
+	state := &State{
+		FilePath:    tmpFile.Name(),
+		UploadID:    "12345",
+		FileSize:    123456789,
+		TrunkSize:   5 * 1024 * 1024, // 5MB
+		TrunkLength: 123456789 / (5 * 1024 * 1024),
+	}
+
+	err = state.Save()
+	require.NoError(t, err, "文件保存失败")
+
+	data, err := os.ReadFile(tmpFile.Name() + ".upload.state")
+	require.NoError(t, err, "文件读取失败")
+	loaded, err := LoadState(data)
+	require.NoError(t, err, "文件解析失败")
+
+	assert.Equal(t, state.UploadID, loaded.UploadID, "UploadID 不一致")
+	assert.Equal(t, state.TrunkLength, loaded.TrunkLength, "分块大小不一致")
+	assert.Equal(t, state.FileSize, loaded.FileSize, "文件大小不一致")
+}
